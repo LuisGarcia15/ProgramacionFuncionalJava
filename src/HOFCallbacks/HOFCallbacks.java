2b764c4f1a0db3bbf253f1b0d80d957d8529c145
Luis Garcia <luis200107@icloud.com>
2024-08-15 23:24:17 -0600

    Añadiendo pequeña implementación sobre las HighOrdenFunction y los Callbacks

---
+++ src/HOFCallbacks/HOFCallbacks.java
@@ -0,0 +1,34 @@
+package HOFCallbacks;
+
+import InterfacesFuncionales.EjemploUno;
+
+public class HOFCallbacks {
+    public static void main(String[] args) {
+
+        /*CallBacks: Aquella función que se pasa como parámetro
+        * a otra función*/
+        System.out.println(HOFParametros(()->{
+            return "Hola mundo desde HOFParametros";
+        }));
+
+        System.out.println("*******************************");
+
+        /*CallBacks: Aquella función que se pasa como parámetro
+         * a otra función*/
+        System.out.println(HOFParametros(HOFreturn()));
+
+    }
+
+    /*High Orden Function que acepta funciones como parámetro*/
+    public static String HOFParametros(EjemploUno funct){
+        String result = funct.getString();
+        return result;
+    }
+
+    /*High Order Function que retorna una función como valor de retorno*/
+    public static EjemploUno HOFreturn(){
+        return () -> {
+            return "Hola mundo desde HOFReturn";
+        };
+    }
+}
